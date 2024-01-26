1b9b1e688dab7d32b432bcef02d496b8f930db87
hdainester <27729552+hdainester@users.noreply.github.com>
2024-01-26 22:20:30 +0100

    Replaced placeholder names/ids with examples

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This repository contains multi modloader project templates for different version
 
 Some configurations have to be adjusted by hand. Search and replace the following terms to your liking:
 
-- <MOD_AUTHOR>
-- <MOD_NAME>
-- <MOD_ID>
+- JoeDoe
+- Example Mod
+- example_mod
--- forge/src/main/java/com/gitlab/srcmc/modtemplate/forge/ModForge.java
+++ forge/src/main/java/com/gitlab/srcmc/modtemplate/forge/ModForge.java
@@ -1,19 +1,19 @@
 /*
- * This file is part of <MOD_NAME>.
+ * This file is part of Example Mod.
  * Copyright (c) 2024, HDainester, All rights reserved.
  *
- * <MOD_NAME> is free software: you can redistribute it and/or modify
+ * Example Mod is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * <MOD_NAME> is distributed in the hope that it will be useful, but
+ * Example Mod is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
  *
  * You should have received a copy of the GNU Lesser General Public License along
- * with <MOD_NAME>. If not, see <http://www.gnu.org/licenses/lgpl>.
+ * with Example Mod. If not, see <http://www.gnu.org/licenses/lgpl>.
  */
 package com.gitlab.srcmc.modtemplate.forge;
 
