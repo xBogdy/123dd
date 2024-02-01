af1bdb771d9b20aa7e3cce8bf4c8e836ecf0f034
hdainester <27729552+hdainester@users.noreply.github.com>
2024-02-01 02:27:48 +0100

    Added common mod class

---
+++ common/src/main/java/com/gitlab/srcmc/my_mod_id/ModCommon.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Example Mod.
+ * Copyright (c) 2024, HDainester, All rights reserved.
+ *
+ * Example Mod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Example Mod is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Example Mod. If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+package com.gitlab.srcmc.my_mod_id;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModCommon {
+    public static final String MOD_ID = "my_mod_id"; // must match mod_id
+    public static final String MOD_NAME = "Example Mod"; // should match mod_name
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);;
+}
