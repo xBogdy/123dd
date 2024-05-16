b33aa4023ea8f7432841e40b0c1199cb8ccaa028
hdainester <27729552+hdainester@users.noreply.github.com>
2024-05-17 01:31:50 +0200

    Some performance adjustments

--- common/src/main/java/com/gitlab/srcmc/rctmod/client/screens/widgets/PlayerInfoWidget.java
+++ common/src/main/java/com/gitlab/srcmc/rctmod/client/screens/widgets/PlayerInfoWidget.java
@@ -4,10 +4,7 @@ import com.gitlab.srcmc.rctmod.ModCommon;
 import com.gitlab.srcmc.rctmod.api.data.pack.TrainerMobData;
 import com.gitlab.srcmc.rctmod.api.data.sync.PlayerState;
 import com.gitlab.srcmc.rctmod.client.ModClient;
-import com.mojang.authlib.GameProfile;
-
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
@@ -23,9 +20,16 @@ public class PlayerInfoWidget extends AbstractWidget {
     private static int W = 224;
     private static int H = 128;
 
+    private static final Component LEVEL_CAP_LABEL = Component.literal("Level Cap");
+    private static final Component TOTAL_DEFEATS_LABEL = Component.literal("Total");
+
     private Font font;
     private TrainerMobData.Type trainerType;
     private boolean allTypes = true;
+    private Component displayName = Component.literal("Trainer").withStyle(ChatFormatting.OBFUSCATED);
+    private Component levelCap = Component.literal("100").withStyle(ChatFormatting.OBFUSCATED);
+    private Component totalDefeats = Component.literal("100").withStyle(ChatFormatting.OBFUSCATED);
+    private ResourceLocation skinLocation = new ResourceLocation("textures/entity/player/wide/steve.png");
 
     public PlayerInfoWidget(Font font) {
         super(X, Y, W, H, Component.empty());
@@ -33,17 +37,24 @@ public class PlayerInfoWidget extends AbstractWidget {
         this.active = false;
     }
 
-    @Override
-    protected void renderWidget(GuiGraphics guiGraphics, int i, int j, float f) {
+    public void tick() {
         var localPlayer = (LocalPlayer)ModClient.get().getLocalPlayer().get();
         var playerState = PlayerState.get(localPlayer);
+        this.displayName = Component.literal(localPlayer.getDisplayName().getString()).withStyle(ChatFormatting.ITALIC);
+        this.levelCap = Component.literal(String.valueOf(playerState.getLevelCap()));
+        this.totalDefeats = Component.literal(String.valueOf(String.format("%9d", this.getTotalDefeats())));
+        this.skinLocation = localPlayer.getSkinTextureLocation();
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics guiGraphics, int i, int j, float f) {
         guiGraphics.blit(new ResourceLocation(ModCommon.MOD_ID, "textures/gui/trainer_card.png"), this.getX(), this.getY(), 0, 0, 224, 128);
-        guiGraphics.drawString(this.font, Component.literal(localPlayer.getDisplayName().getString()).withStyle(ChatFormatting.ITALIC), this.getX() + 12, this.getY() + 12, 16);
-        guiGraphics.drawString(this.font, Component.literal("Level Cap"), this.getX() + 12, this.getY() + 108, 16);
-        guiGraphics.drawString(this.font, Component.literal(String.valueOf(playerState.getLevelCap())).withStyle(ChatFormatting.ITALIC), this.getX() + 72, this.getY() + 108, 16);
-        guiGraphics.drawString(this.font, Component.literal("Total"), this.getX() + 100, this.getY() + 108, 16);
-        guiGraphics.drawString(this.font, Component.literal(String.valueOf(String.format("%9d", this.getTotalDefeats()))).withStyle(ChatFormatting.ITALIC), this.getX() + 116, this.getY() + 108, 16);
-        PlayerFaceRenderer.draw(guiGraphics, localPlayer.getSkinTextureLocation(), this.getX() + 8 + 4, this.getY() + 32, 72);
+        guiGraphics.drawString(this.font, this.displayName, this.getX() + 12, this.getY() + 12, 16);
+        guiGraphics.drawString(this.font, LEVEL_CAP_LABEL, this.getX() + 12, this.getY() + 108, 16);
+        guiGraphics.drawString(this.font, this.levelCap, this.getX() + 72, this.getY() + 108, 16);
+        guiGraphics.drawString(this.font, TOTAL_DEFEATS_LABEL, this.getX() + 100, this.getY() + 108, 16);
+        guiGraphics.drawString(this.font, this.totalDefeats, this.getX() + 116, this.getY() + 108, 16);
+        PlayerFaceRenderer.draw(guiGraphics, this.skinLocation, this.getX() + 8 + 4, this.getY() + 32, 72);
     }
 
     public void setTrainerType(TrainerMobData.Type trainerType) {
@@ -65,7 +76,7 @@ public class PlayerInfoWidget extends AbstractWidget {
 
         var localPlayer = (LocalPlayer)ModClient.get().getLocalPlayer().get();
         var playerState = PlayerState.get(localPlayer);
-        return playerState.getTrainerDefeatCounts().values().stream().mapToLong(i -> i).reduce(0, Math::addExact);
+        return playerState.getTypeDefeatCounts().values().stream().mapToLong(i -> i).reduce(0, Math::addExact);
     }
 
     private long getTotalDefeats(TrainerMobData.Type type) {
